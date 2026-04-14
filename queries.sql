0d0a07adc8165a7aa859d682811a1e15f2d1415f
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-14 11:22:02 +0530

    love data

--- queries.sql
+++ queries.sql
@@ -6,3 +6,6 @@ FROM station;
 -- this sql query explains the use of NOT keyword in the mysql
 
 SELECT * from station where not city = 'Berlin';
+-- the above sql quey can also be written as :
+
+select * from station where city != 'Berlin';
