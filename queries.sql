910664201b202145c234d9fb90632c14a605d88e
Rishabh <rishabhdubey1472004@gmail.com>
2026-04-15 09:08:19 +0530

    love data

--- queries.sql
+++ queries.sql
@@ -45,3 +45,11 @@ SELECT (
            LIMIT 1
     ) AS num;
 
+-- the following query is an example of inner join
+select e2.name as Employee
+from Employee e1
+         inner join Employee e2
+                    on e1.id = e2.managerId
+where e1.salary < e2.salary;
+
+
